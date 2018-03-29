64c1c8705cebc0d2da09eda35770392b45af9a48
USER-20180318GK\Administrator <yangtengfei1314@gmail.com>
2018-03-29 16:40:50 +0800

    更改rsa加密公钥

--- QuanMB/app/src/main/java/com/jingnuo/quanmb/data/Staticdata.java
+++ QuanMB/app/src/main/java/com/jingnuo/quanmb/data/Staticdata.java
@@ -8,10 +8,10 @@ public class Staticdata {
     /**
      * 登陆时候Rsa加密的公钥
      */
-    public  static  String PUBLIC_KEY_STR="MFswDQYJKoZIhvcNAQEBBQADSgAwRwJAUwbM2XEUCvdSisGwKra11AE1FUf37ViExqCdXiZGzaAprS+eKfrXqTn9yru8XAZ3ORTz1T5sCZlBVCe4eRGeCQIDAQAB";
+    public  static  String PUBLIC_KEY_STR="MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCXtNyajfT+Z1+p4wLQXIzlbiSJUXG8lhS9ZeCjDHXclZa6G2xUWvliVXSPc8gAuWC1yz1OibXwspGq48kzqPoyqlNr37uBBjUArquKD/ky0BaADOeD+eHy7jnkXdshSUZAdYpIYEKuM3y+/9qSI+qkJZV/Z8xHKDLUIXYToL/AWwIDAQAB";
 
     //用户是否已经登陆  true表示登录   flase表示未登录    待解决   退出程序后还为true
-    public  static boolean isLogin=true;//默认为未登录
+    public  static boolean isLogin=false;//默认为未登录
 
 
 
