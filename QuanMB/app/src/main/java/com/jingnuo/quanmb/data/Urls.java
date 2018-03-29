0ca5e674a68200eadb2a50d928f304b723b5a2b9
USER-20180318GK\Administrator <yangtengfei1314@gmail.com>
2018-03-29 17:50:20 +0800

    登陆接口测试通过

---
+++ QuanMB/app/src/main/java/com/jingnuo/quanmb/data/Urls.java
@@ -0,0 +1,9 @@
+package com.jingnuo.quanmb.data;
+
+/**
+ * Created by Administrator on 2018/3/29.
+ */
+
+public class Urls {
+    public  static String login="http://192.168.1.184:8080/QuanMinBang/login/doLogin";
+}
--- QuanMB/app/src/main/java/com/jingnuo/quanmb/fargment/Fragment_acountLogin.java
+++ QuanMB/app/src/main/java/com/jingnuo/quanmb/fargment/Fragment_acountLogin.java
@@ -12,17 +12,23 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.jingnuo.quanmb.Interface.Interface_volley_respose;
 import com.jingnuo.quanmb.activity.FindPasswordActivity;
 import com.jingnuo.quanmb.activity.MainActivity;
 import com.jingnuo.quanmb.activity.RegisterActivity;
+import com.jingnuo.quanmb.data.Urls;
 import com.jingnuo.quanmb.quanmb.R;
+import com.jingnuo.quanmb.utils.InstalltionId;
 import com.jingnuo.quanmb.utils.LogUtils;
 import com.jingnuo.quanmb.utils.RsaUtils;
 import com.jingnuo.quanmb.utils.ToastUtils;
+import com.jingnuo.quanmb.utils.Volley_Utils;
 
 import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.jingnuo.quanmb.data.Staticdata.PUBLIC_KEY_STR;
 import static com.jingnuo.quanmb.data.Staticdata.isLogin;
@@ -41,6 +47,12 @@ public class Fragment_acountLogin extends Fragment {
     //数据
     String account = "";
     String password = "";
+    String UUID = "";
+    String publicEncryptedResult = "";
+
+    //对象
+    PublicKey publicKey;
+    Map map_login;
 
 
     @Nullable
@@ -49,10 +61,16 @@ public class Fragment_acountLogin extends Fragment {
 
         rootview = inflater.inflate(R.layout.fragment_acount_login, container, false);
         initview();
+        initdata();
         initlinster();
 
 
         return rootview;
+
+    }
+
+    private void initdata() {
+        UUID = InstalltionId.id(getActivity());//第一次运行生成一个id
 
     }
 
@@ -65,20 +83,22 @@ public class Fragment_acountLogin extends Fragment {
                 if (account.equals("") || password.equals("")) {
                     ToastUtils.showToast(getActivity(), "请填写账号密码");
                 } else {
+                    //     加密过程
                     //获取公钥
-                    PublicKey publicKey = RsaUtils.keyStrToPublicKey(PUBLIC_KEY_STR);
-
+                    publicKey = RsaUtils.keyStrToPublicKey(PUBLIC_KEY_STR);
                     //公钥加密结果
-                    String publicEncryptedResult = RsaUtils.encryptDataByPublicKey(password.getBytes(), publicKey);
+                    publicEncryptedResult = RsaUtils.encryptDataByPublicKey(password.getBytes(), publicKey);
 //                    //私钥解密结果
 //                    String privateDecryptedResult = RsaUtils.decryptedToStrByPrivate(publicEncryptedResult,privateKey);
 
                     LogUtils.LOG("ceshi", publicEncryptedResult + "1111111111", "fragment_account");
-                    //登陆成功后 设置全局变量islogin为 true
-                    isLogin = true;
-                    Intent intent_login = new Intent(getActivity(), MainActivity.class);
-                    getActivity().startActivity(intent_login);
 
+                    map_login = new HashMap();
+                    map_login.put("username", account);
+                    map_login.put("password", publicEncryptedResult);
+                    map_login.put("uuid", UUID);
+                    //登陆成功后 设置全局变量islogin为 true
+                    requestlogin(map_login);//登陆请求
                 }
 
 
@@ -93,11 +113,34 @@ public class Fragment_acountLogin extends Fragment {
         });
     }
 
+    private void requestlogin(Map map) {
+
+
+        new Volley_Utils(new Interface_volley_respose() {
+            @Override
+            public void onSuccesses(String respose) {
+                LogUtils.LOG("ceshi", respose + "1111111111", "fragment_account");
+                isLogin = true;
+                Intent intent_login = new Intent(getActivity(), MainActivity.class);
+                getActivity().startActivity(intent_login);
+
+            }
+
+            @Override
+            public void onError(int error) {
+
+            }
+        }).postHttp(Urls.login, getActivity(), 1, map);
+    }
+
     private void initview() {
         medit_account = rootview.findViewById(R.id.edit_account);
         medit_password = rootview.findViewById(R.id.edit_password);
         mButton_login = rootview.findViewById(R.id.button_login);
         mtextview_forgotpassword = rootview.findViewById(R.id.textview_forgotpassword);
     }
+
+
+
 
 }
