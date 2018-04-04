caf35736a8cb95749bf4ffb28e667f5788909025
USER-20180318GK\Administrator <yangtengfei1314@gmail.com>
2018-04-04 18:26:15 +0800

    找专业界面完成一半

---
+++ QuanMB/app/src/main/java/com/jingnuo/quanmb/Adapter/Adapter_classification_right.java
@@ -0,0 +1,51 @@
+package com.jingnuo.quanmb.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.jingnuo.quanmb.quanmb.R;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018/4/4.
+ */
+
+public class Adapter_classification_right extends BaseAdapter {
+    List<String> mData;
+    Context mContext;
+    LayoutInflater mInflater;
+
+    public Adapter_classification_right(List mDatas, Context mContext) {
+        super(mDatas, mContext);
+        this.mData = mDatas;
+        this.mContext = mContext;
+        mInflater = LayoutInflater.from(mContext);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        viewholder holder = null;
+
+        if (convertView == null) {
+            holder = new viewholder();
+            convertView = mInflater.inflate(R.layout.item_gridview_classification_right, null, false);
+            holder.mTextview = convertView.findViewById(R.id.textview_grid_right);
+            convertView.setTag(holder);
+        } else {
+            holder = (viewholder) convertView.getTag();
+        }
+        holder.mTextview.setText(mData.get(position));
+
+
+        return convertView;
+    }
+
+    class viewholder {
+        TextView mTextview;
+
+    }
+}
--- QuanMB/app/src/main/java/com/jingnuo/quanmb/fargment/Fragment_still.java
+++ QuanMB/app/src/main/java/com/jingnuo/quanmb/fargment/Fragment_still.java
@@ -6,9 +6,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.jingnuo.quanmb.Adapter.Adapter_classification_left;
+import com.jingnuo.quanmb.Adapter.Adapter_classification_right;
+import com.jingnuo.quanmb.customview.MyGridView;
+import com.jingnuo.quanmb.customview.MyListView;
 import com.jingnuo.quanmb.quanmb.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Administrator on 2018/3/20.
@@ -18,14 +29,71 @@ public class Fragment_still  extends Fragment{
     View rootview;
     //控件
     EditText mEdit_serch;//搜索关键字
+    TextView mtextview_classification_title;
+
+    ListView mListview_left;
+    MyGridView mGridview_right;
+
+    //数据
+
+
+    //对象
+    Adapter_classification_left mAdapter_classification_left;
+
+    Adapter_classification_right mAdapter_classification_right;
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootview=inflater.inflate(R.layout.fragment_still,container,false);
-
+        initview();
+        initdata();
+        initlistenner();
 
         return rootview;
+    }
+
+    private void initlistenner() {
+        mListview_left.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                mAdapter_classification_left.setSelectedPosition(i);
+                mAdapter_classification_left.notifyDataSetInvalidated();
+            }
+        });
+    }
+
+    private void initdata() {
+        List<String> mList_left=new ArrayList<>();
+        mList_left.add("热门任务");
+        mList_left.add("配送");
+        mList_left.add("运输");
+        mList_left.add("跑腿");
+        mList_left.add("维修");
+        mList_left.add("导游");
+        mList_left.add("辅导");
+        mList_left.add("托教");
+        mAdapter_classification_left=new Adapter_classification_left(mList_left,getActivity());
+        mListview_left.setAdapter(mAdapter_classification_left);
+
+
+        List<String> mList_right=new ArrayList<>();
+        mList_right.add("汽车美容");
+        mList_right.add("汽车维修");
+        mList_right.add("跑腿代办");
+        mList_right.add("宠物");
+        mList_right.add("漂流");
+        mList_right.add("代驾");
+        mList_right.add("租车位");
+        mList_right.add("月嫂");
+        mAdapter_classification_right=new Adapter_classification_right(mList_right,getActivity());
+        mGridview_right.setAdapter(mAdapter_classification_right);
+    }
+
+    private void initview() {
+        mtextview_classification_title=rootview.findViewById(R.id.textviw_classification);
+        mListview_left=rootview.findViewById(R.id.gridview_left);
+        mGridview_right=rootview.findViewById(R.id.gridview_right);
     }
 }
