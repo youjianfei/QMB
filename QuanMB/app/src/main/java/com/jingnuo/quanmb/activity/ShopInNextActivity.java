bcbc8f4d37ef989908f1efba715d9af3411701c6
USER-20180318GK\Administrator <yangtengfei1314@gmail.com>
2018-04-08 11:37:17 +0800

    还价消息界面  的一个完成

--- QuanMB/app/src/main/java/com/jingnuo/quanmb/activity/ShopInNextActivity.java
+++ QuanMB/app/src/main/java/com/jingnuo/quanmb/activity/ShopInNextActivity.java
@@ -1,26 +1,20 @@
 package com.jingnuo.quanmb.activity;
 
-import android.content.Intent;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.jingnuo.quanmb.data.Staticdata;
 import com.jingnuo.quanmb.quanmb.R;
 
-public class ShopInActivity extends BaseActivityother {
+public class ShopInNextActivity extends BaseActivityother {
     //布局
     private LinearLayout mLineatlayout_addview;
 
-    //控件
-    private Button mButton_submit;
-
-
-
     @Override
     public int setLayoutResID() {
-        return R.layout.activity_shop_in;
+        return R.layout.activity_shop_in_next;
     }
 
     @Override
@@ -32,7 +26,7 @@ public class ShopInActivity extends BaseActivityother {
     protected void initData() {
         //添加头部3个步骤的图片
         ImageView imageview_shopin=new ImageView(this);
-        imageview_shopin.setBackgroundResource(R.mipmap.shopin_1);
+        imageview_shopin.setBackgroundResource(R.mipmap.shopin_3);
         LinearLayout.LayoutParams mLayoutparams=new LinearLayout.LayoutParams(Staticdata.ScreenWidth, (int) (Staticdata.ScreenWidth*0.27));
         imageview_shopin.setLayoutParams(mLayoutparams);
         mLineatlayout_addview.addView(imageview_shopin);
@@ -40,22 +34,11 @@ public class ShopInActivity extends BaseActivityother {
 
     @Override
     protected void initListener() {
-        mButton_submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent  intent_shopIn_next=new Intent(ShopInActivity.this,ShopInNextActivity.class);
-                startActivity(intent_shopIn_next);
-            }
-        });
-
 
     }
 
     @Override
     protected void initView() {
         mLineatlayout_addview=findViewById(R.id.linrarlayout_shop);
-        mButton_submit=findViewById(R.id.button_submit);
-
-
     }
 }
--- QuanMB/app/src/main/java/com/jingnuo/quanmb/fargment/Fragment_message.java
+++ QuanMB/app/src/main/java/com/jingnuo/quanmb/fargment/Fragment_message.java
@@ -1,12 +1,15 @@
 package com.jingnuo.quanmb.fargment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
+import com.jingnuo.quanmb.activity.BargainActivity;
 import com.jingnuo.quanmb.quanmb.R;
 
 /**
@@ -17,12 +20,31 @@ public class Fragment_message extends Fragment {
     View rootview;
     //控件
 
+    RelativeLayout mRelativelayout_bargain;
+
+
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootview=inflater.inflate(R.layout.fragment_message,container,false);
+        initview();
+        initlistener();
         return  rootview;
+    }
+
+    private void initlistener() {
+        mRelativelayout_bargain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intend_bargain=new Intent(getActivity(), BargainActivity.class);
+                getActivity().startActivity(intend_bargain);
+            }
+        });
+    }
+
+    private void initview() {
+        mRelativelayout_bargain=rootview.findViewById(R.id.relativelayout_Kanprice);
     }
 }
