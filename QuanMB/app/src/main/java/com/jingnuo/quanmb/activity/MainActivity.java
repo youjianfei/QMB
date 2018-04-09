a02aa338e9c304222ba617478be576c1258294b6
USER-20180318GK\Administrator <yangtengfei1314@gmail.com>
2018-04-09 16:44:32 +0800

    获取了当前位置的经纬度

--- QuanMB/app/src/main/java/com/jingnuo/quanmb/activity/MainActivity.java
+++ QuanMB/app/src/main/java/com/jingnuo/quanmb/activity/MainActivity.java
@@ -11,6 +11,10 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.jaeger.library.StatusBarUtil;
 import com.jingnuo.quanmb.fargment.Fragment_message;
 import com.jingnuo.quanmb.fargment.Fragment_person;
@@ -33,8 +37,13 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
     FragmentManager fragmetnmanager;
     FragmentTransaction transaction;
     //控件
-    private RelativeLayout mRelativeLayout_square, mRelativeLayout_still,  mRelativeLayout_message, mRelativeLayout_person;
+    private RelativeLayout mRelativeLayout_square, mRelativeLayout_still, mRelativeLayout_message, mRelativeLayout_person;
     ImageView mImage_release;
+
+
+    //百度地图相关
+    public LocationClient mLocationClient = null;
+    private MyLocationListener myListener = new MyLocationListener();
 
 
     @Override
@@ -42,6 +51,12 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         StatusBarUtil.setColor(this, getResources().getColor(R.color.statebar), 0);//状态栏颜色
+        mLocationClient = new LocationClient(getApplicationContext());
+        //声明LocationClient类
+        mLocationClient.registerLocationListener(myListener);
+        //注册监听函数
+
+        setmapdata();// 百度地图配置参数
         initview();
         initdata();
         initlinstenner();
@@ -50,15 +65,58 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
 
     }
 
+    private void setmapdata() {
+        LocationClientOption option = new LocationClientOption();
+
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        option.setCoorType("bd09ll");
+        //可选，设置返回经纬度坐标类型，默认gcj02
+        //gcj02：国测局坐标；
+        //bd09ll：百度经纬度坐标；
+        //bd09：百度墨卡托坐标；
+        //海外地区定位，无需设置坐标类型，统一返回wgs84类型坐标
+        option.setScanSpan(0);
+        //可选，设置发起定位请求的间隔，int类型，单位ms
+        //如果设置为0，则代表单次定位，即仅定位一次，默认为0
+        //如果设置非0，需设置1000ms以上才有效
+
+        option.setOpenGps(true);
+        //可选，设置是否使用gps，默认false
+        //使用高精度和仅用设备两种定位模式的，参数必须设置为true
+
+        option.setLocationNotify(true);
+        //可选，设置是否当GPS有效时按照1S/1次频率输出GPS结果，默认false
+
+        option.setIgnoreKillProcess(false);
+        //可选，定位SDK内部是一个service，并放到了独立进程。
+        //设置是否在stop的时候杀死这个进程，默认（建议）不杀死，即setIgnoreKillProcess(true)
+
+        option.SetIgnoreCacheException(false);
+        //可选，设置是否收集Crash信息，默认收集，即参数为false
+
+        option.setWifiCacheTimeOut(5 * 60 * 1000);
+        //可选，7.2版本新增能力
+        //如果设置了该接口，首次启动定位时，会先判断当前WiFi是否超出有效期，若超出有效期，会先重新扫描WiFi，然后定位
+
+        option.setEnableSimulateGps(false);
+        //可选，设置是否需要过滤GPS仿真结果，默认需要，即参数为false
+
+        mLocationClient.setLocOption(option);
+        //mLocationClient为第二步初始化过的LocationClient对象
+        //需将配置好的LocationClientOption对象，通过setLocOption方法传递给LocationClient对象使用
+        //更多LocationClientOption的配置，请参照类参考中LocationClientOption类的详细说明
+    }
+
     public void initview() {
         mRelativeLayout_square = findViewById(R.id.relative_square);
         mRelativeLayout_still = findViewById(R.id.relative_still);
         mRelativeLayout_message = findViewById(R.id.relative_message);
         mRelativeLayout_person = findViewById(R.id.relative_person);
-        mImage_release=findViewById(R.id.image_release);
+        mImage_release = findViewById(R.id.image_release);
     }
 
     public void initdata() {
+        mLocationClient.start();//百度地图获取当前位置经纬度
         mFragment_square = new Fragment_square();
         fragmetnmanager = getSupportFragmentManager();
         transaction = fragmetnmanager.beginTransaction();
@@ -113,11 +171,11 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
 
                 break;
             case R.id.image_release:
-                if(isLogin){
-                    Intent intent_issue=new Intent(this,IssueTaskActivity.class);
+                if (isLogin) {
+                    Intent intent_issue = new Intent(this, IssueTaskActivity.class);
                     startActivity(intent_issue);
-                }else{
-                    Intent intent_login=new Intent(this,LoginActivity.class);
+                } else {
+                    Intent intent_login = new Intent(this, LoginActivity.class);
                     startActivity(intent_login);
                 }
 
@@ -133,8 +191,8 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
                 }
                 break;
             case R.id.relative_person:
-                LogUtils.LOG("ceshi", "个人中心被点击"+isLogin, "mainactivity");
-                if(isLogin){
+                LogUtils.LOG("ceshi", "个人中心被点击" + isLogin, "mainactivity");
+                if (isLogin) {
                     ChangeBottomButton(mRelativeLayout_person);
                     if (mFragment_person == null) {
                         mFragment_person = new Fragment_person();
@@ -142,11 +200,10 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
                     } else {
                         transaction.show(mFragment_person).commit();
                     }
-                }else {
-                    Intent intent_login=new Intent(this,LoginActivity.class);
+                } else {
+                    Intent intent_login = new Intent(this, LoginActivity.class);
                     startActivity(intent_login);
                 }
-
 
 
                 break;
@@ -168,6 +225,7 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
             transaction.hide(mFragment_person);
         }
     }
+
     private void ChangeBottomButton(RelativeLayout rl) {//控制底部按钮颜色的变化
         mRelativeLayout_square.getChildAt(0).setSelected(false);
         mRelativeLayout_square.getChildAt(1).setSelected(false);
@@ -201,6 +259,27 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
             finish();
         }
     }
+    public class MyLocationListener extends BDAbstractLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location){
+            //此处的BDLocation为定位结果信息类，通过它的各种get方法可获取定位相关的全部结果
+            //以下只列举部分获取经纬度相关（常用）的结果信息
+            //更多结果信息获取说明，请参照类参考中BDLocation类中的说明
+
+            double latitude = location.getLatitude();    //获取纬度信息
+            double longitude = location.getLongitude();    //获取经度信息
+            float radius = location.getRadius();    //获取定位精度，默认值为0.0f
+
+            String coorType = location.getCoorType();
+            //获取经纬度坐标类型，以LocationClientOption中设置过的坐标类型为准
+
+            int errorCode = location.getLocType();
+            //获取定位类型、定位错误返回码，具体信息可参照类参考中BDLocation类中的说明
+
+            LogUtils.LOG("ceshi","纬度信息"+latitude+"经度信息"+longitude+"定位精度"+radius+"经纬度坐标类型"+coorType+"错误返回码"+errorCode,"mainactivity");
+        }
+    }
+
 }
 
 
