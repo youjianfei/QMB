f4653d7014b64ace6ea6fa5f34ded1c62a4c7ec4
USER-20180318GK\Administrator <yangtengfei1314@gmail.com>
2018-04-11 10:52:23 +0800

    商家中心界面完成

---
+++ QuanMB/app/src/main/java/com/jingnuo/quanmb/activity/ShopCenterActivity.java
@@ -0,0 +1,34 @@
+package com.jingnuo.quanmb.activity;
+
+
+import com.jingnuo.quanmb.quanmb.R;
+
+
+public class ShopCenterActivity extends BaseActivityother {
+
+
+    @Override
+    public int setLayoutResID() {
+        return R.layout.activity_shop_center;
+    }
+
+    @Override
+    protected void setData() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
