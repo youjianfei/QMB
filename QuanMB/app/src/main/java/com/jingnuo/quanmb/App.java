5d69177488e1ab83b81e4e821a9cc5bfd2d8a7d4
USER-20180318GK\Administrator <yangtengfei1314@gmail.com>
2018-04-17 12:04:23 +0800

    集成配置友盟

--- QuanMB/app/src/main/java/com/jingnuo/quanmb/App.java
+++ QuanMB/app/src/main/java/com/jingnuo/quanmb/App.java
@@ -3,6 +3,10 @@ package com.jingnuo.quanmb;
 import android.app.Application;
 
 import com.baidu.mapapi.SDKInitializer;
+import com.umeng.commonsdk.UMConfigure;
+import com.umeng.socialize.Config;
+import com.umeng.socialize.UMShareAPI;
+import com.umeng.socialize.UMShareConfig;
 
 /**
  * Created by Administrator on 2018/4/9.
@@ -13,5 +17,8 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         SDKInitializer.initialize(getApplicationContext());//初始化Baidu地图
+        UMConfigure.init(this,"5ad55925f43e4835c1000064"
+                ,"umeng",UMConfigure.DEVICE_TYPE_PHONE,"");//58edcfeb310c93091c000be2 5965ee00734be40b580001a0
+        UMConfigure.setLogEnabled(true);
     }
 }
