687bc4064720fe2d50390fc88d8fa9e38aed3e65
USER-20180318GK\Administrator <yangtengfei1314@gmail.com>
2018-05-28 13:36:50 +0800

    友盟换为手动导入

--- QuanMB/app/src/main/java/com/jingnuo/quanmb/wxapi/WXEntryActivity.java
+++ QuanMB/app/src/main/java/com/jingnuo/quanmb/wxapi/WXEntryActivity.java
@@ -3,6 +3,8 @@ package com.jingnuo.quanmb.wxapi;
 
 //import com.umeng.weixin.callback.WXCallbackActivity;
 
+//import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
 import com.umeng.socialize.weixin.view.WXCallbackActivity;
 
 public class WXEntryActivity extends WXCallbackActivity {
