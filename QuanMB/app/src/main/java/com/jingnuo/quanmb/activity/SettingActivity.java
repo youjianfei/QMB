1e5fd9375a5a8c8801b1add1c616ac13d2a24533
USER-20180318GK\Administrator <yangtengfei1314@gmail.com>
2018-07-12 09:34:44 +0800

    添加了清除缓存功能

--- QuanMB/app/src/main/java/com/jingnuo/quanmb/activity/SettingActivity.java
+++ QuanMB/app/src/main/java/com/jingnuo/quanmb/activity/SettingActivity.java
@@ -10,8 +10,11 @@ import android.widget.TextView;
 
 import com.jingnuo.quanmb.data.Staticdata;
 import com.jingnuo.quanmb.quanmb.R;
+import com.jingnuo.quanmb.utils.DataCleanManager;
+import com.jingnuo.quanmb.utils.LogUtils;
 import com.jingnuo.quanmb.utils.SharedPreferencesUtils;
 import com.jingnuo.quanmb.utils.ToastUtils;
+import com.jingnuo.quanmb.utils.Utils;
 
 import java.io.File;
 
@@ -23,6 +26,7 @@ public class SettingActivity extends BaseActivityother {
     Button mButton_logout;
     TextView mTextview_cleancache;
     TextView mTextview_suggest;
+    TextView mTextview_textview_cleancacheSize;
     @Override
     public int setLayoutResID() {
         return R.layout.activity_setting;
@@ -30,7 +34,11 @@ public class SettingActivity extends BaseActivityother {
 
     @Override
     protected void setData() {
-
+        try {
+            mTextview_textview_cleancacheSize.setText( DataCleanManager. getTotalCacheSize(SettingActivity.this));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -50,6 +58,7 @@ public class SettingActivity extends BaseActivityother {
         mButton_logout=findViewById(R.id.button_logout);
         mTextview_cleancache=findViewById(R.id.textview_cleancache);
         mTextview_suggest=findViewById(R.id.textview_suggest);
+        mTextview_textview_cleancacheSize=findViewById(R.id.textview_cleancacheSize);
     }
     File root;//分享的图片要放的文件夹
     @Override
@@ -66,14 +75,20 @@ public class SettingActivity extends BaseActivityother {
                 break;
 
             case R.id.textview_cleancache:
-                ToastUtils.showToast(this,"清除缓存成功");
+
                 root = new File(Environment.getExternalStorageDirectory() + "/picyasuo/");//找到根目录下DICM文件夹
                 if (!root.exists()) {
                     root.mkdirs();
                 }
                 deleteAllFiles(root);//删除上次分享的残余图片
-
-            break;
+                try {
+                    DataCleanManager.clearAllCache(SettingActivity.this);
+                    ToastUtils.showToast(this,"清除缓存成功");
+                    mTextview_textview_cleancacheSize.setText( DataCleanManager. getTotalCacheSize(SettingActivity.this));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
             case R.id.textview_suggest:
                 Intent intend_suggest=new Intent(this,SuggestActivity.class);
                 startActivity(intend_suggest);
